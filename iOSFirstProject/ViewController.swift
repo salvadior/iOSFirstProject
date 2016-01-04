b8ef9f9f09d238fb576b1f34f2964d4078a5bae7
dennis-geekman <rainwoodhere@icloud.com>
2016-01-04 14:54:10 +0300

    Added files of project repository iOSFirstProject

--- iOSFirstProject/ViewController.swift
+++ iOSFirstProject/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var topLabel: UILabel!
+    @IBOutlet var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ChangeLabelName(sender: AnyObject) {
+        topLabel.text = "Hello " + textField.text! + ", welcome to iOS development!"
+    }
 
+    @IBAction func CloseThisApplication(sender: AnyObject) {
+        exit(EXIT_SUCCESS);
+    }
+    @IBAction func KeyboardDissmiss(sender: AnyObject) {
+        self.resignFirstResponder()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
